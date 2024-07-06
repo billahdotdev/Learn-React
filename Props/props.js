29defb774938afcefa84f13ed9af1ec9e8ac487c
Masum Billah <emasumbillah@gmail.com>
2024-07-06 12:27:08 +0800

    props in react

---
+++ Props/props.js
@@ -0,0 +1,21 @@
+//Props are a way to pass data from one component to another in React.
+
+/*You can pass props from a parent component to a child component by 
+adding attributes to the child component's JSX tag.*/
+
+// ParentComponent.jsx
+
+import ChildComponent from './ChildComponent';
+
+function ParentComponent() {
+  return <ChildComponent name="John" age={25} />;
+}
+
+
+//Accessing Props: Inside the child component, props are accessed as an object.
+
+// ChildComponent.jsx
+
+function ChildComponent(props) {
+  return <p>Name: {props.name}, Age: {props.age}</p>;
+}
