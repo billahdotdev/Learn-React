7e628f67caf527479fff433c325a374800d7263e
Masum Billah <emasumbillah@gmail.com>
2024-07-07 07:59:19 +0800

    Props, Destructuring, Props Immutability, Types of Props, and Defaault Props

--- Props/Destructuring-Props.js
+++ Props/Destructuring-Props.js
@@ -1,20 +1,23 @@
-/*Destructuring Props
-Using Destructuring: Alternatively, 
+//Destructuring Props
+
+/*Alternatively,
 you can destructure props directly in the function signature for cleaner code.
 */
-
 
 // ParentComponent.jsx
 
 import ChildComponent from './ChildComponent';
 
 function ParentComponent() {
-  return <ChildComponent name="John" age={25} />;
+    return <ChildComponent name="John" age={25} />;
 }
-
 
 // ChildComponent.jsx
 
 function ChildComponent({ name, age }) {
-  return <p>Name: {name}, Age: {age}</p>;
+    return (
+        <p>
+            Name: {name}, Age: {age}
+        </p>
+    );
 }
--- Props/Types-Of-Props.js
+++ Props/Types-Of-Props.js
@@ -1,4 +1,4 @@
-//Types
+//Types of Props
 
 /*
 Props can be any type of data
