f3398263c0249d98fb78c0a8fe0893ed99d90b90
Masum Billah <emasumbillah@gmail.com>
2024-07-10 10:26:04 +0800

    commited

---
+++ Props/Passing Arrays & Objects.js
@@ -0,0 +1,12 @@
+// Passing Arrays as Props
+
+// Parent Component (Sender):
+
+/*Assume you have an array defined in your
+parent component and you
+want to pass it to a child component.*/
+
+// Child Component (Receiver):
+
+/*Receive the array using props
+in the child component.*/
